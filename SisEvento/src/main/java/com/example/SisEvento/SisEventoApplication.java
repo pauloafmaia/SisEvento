fd4ba6214dfcabb4fc8e360e875b202c6a13c452
paulo maia <pauloafmaia9@gmail.com>
2022-12-21 13:53:09 -0300

    Initial commit

---
+++ SisEvento/src/main/java/com/example/SisEvento/SisEventoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SisEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisEventoApplication.class, args);
+	}
+
+}
---
+++ SisEvento/src/test/java/com/example/SisEvento/SisEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SisEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
