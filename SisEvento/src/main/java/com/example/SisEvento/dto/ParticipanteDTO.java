b3f70e1e69b1c96266e84ede49bdb913ae15ed10
paulo maia <pauloafmaia9@gmail.com>
2022-12-21 14:20:11 -0300

    Criação Participante e ParticipanteDTO

---
+++ SisEvento/src/main/java/com/example/SisEvento/dto/ParticipanteDTO.java
@@ -0,0 +1,24 @@
+package com.example.SisEvento.dto;
+
+import lombok.Data;
+
+@Data
+public class ParticipanteDTO {
+
+    private Long id;
+    private String nome;
+    private String apelido;
+    private String dataNascimento;
+    private String sexo;
+    private String email;
+    private String whatsApp;
+    private String nomeMae;
+    private String foneMae;
+    private String nomePai;
+    private String fonePai;
+    private String pessoaContato;
+    private String foneContato;
+    private String medicacao;
+    private String alergia;
+    private String restricaoAlimentar;
+}
